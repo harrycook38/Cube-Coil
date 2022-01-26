c246509cb82d28adfbf367aa79e865da2dd57821
Harry <ppyhc2@nottingham.ac.uk>
2022-01-26 15:43:33 +0000

    Onwards

---
+++ Square.m
@@ -0,0 +1,13 @@
+clear all
+close all
+clc
+
+d = 0.4;
+a = linspace(0,2*pi,100);
+
+A = linspace(-d/2,d/2,100);
+B = linspace(-d/2,d/2,100);
+
+dist = [A; B];
+
+points = [(A./cos(a)),(B./cos(a))];
