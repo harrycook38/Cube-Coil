36ccf294a27b02e6b881a7a9a13c0557871eeb72
Harry <ppyhc2@nottingham.ac.uk>
2022-04-06 13:09:06 +0100

    Data Analysis Practice

---
+++ Lab Stuff/Code/HandWillRead.m
@@ -0,0 +1,92 @@
+clear all
+close all
+clc
+
+addpath('Code/')
+addpath('Data/5-4-22')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+fname = 'QZFM_6.lvm';
+
+%Load in time stamps and data
+[rtime,rdata] = eb_read_lvm(fname);
+
+fgmout = rdata(:,1:3); %FGM Data
+Iout = rdata(:,4:6)./10; %Current out from loops
+
+figure(1)
+subplot(2,1,1)
+Bout = 0.1e-6*fgmout; %Field (Conversion 0.1 uT/V)
+Bcor = Bout - mean(Bout,1); %Correct for Vertical Offset
+plot(rtime,Bcor)
+title("offset-corrected Magnetic Field")
+
+subplot(2,1,2)
+plot(rtime,Iout)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure(2)
+Fs = 1213;
+N = length(Bcor);
+
+subplot(3,1,1)
+xfti = fft(Bcor(:,1));
+xft = xfti(1:N/2+1);
+psdx = (1/(Fs*N)).*xft.*conj(xft);
+psdx(2:end-1) = 2*psdx(2:end-1);
+freq = 0:Fs/N:Fs/2;
+plot(freq,10*log10(psdx))
+
+subplot(3,1,2)
+yfti = fft(Bcor(:,2));
+yft = yfti(1:N/2+1);
+psdy = (1/(Fs*N)).*yft.*conj(yft);
+psdy(2:end-1) = 2*psdy(2:end-1);
+plot(freq,10*log10(psdy))
+
+subplot(3,1,3)
+zfti = fft(Bcor(:,3));
+zft = zfti(1:N/2+1);
+psdz = (1/(Fs*N)).*zft.*conj(zft);
+psdz(2:end-1) = 2*psdz(2:end-1);
+plot(freq,10.*log10(psdz))
+
+%Filter 
+mu = 10; %Target frequency
+sd = 0.5;
+
+a = (1./sqrt(2.*pi.*sd));
+
+gau = a.*exp(-(freq-mu).^8/(2*sd^2));
+
+filtx = gau(1,:)'.*psdx;
+invx = ifft(filtx);
+
+filty = gau(2,:)'.*psdy;
+invy = ifft(filty);
+
+filtz = gau(3,:)'.*psdz;
+invz = ifft(filtz);
+
+figure(3)
+subplot(3,1,1)
+plot(rtime(1:2:end),invx)
+
+subplot(3,1,2)
+plot(rtime(1:2:end),invy)
+
+subplot(3,1,3)
+plot(rtime(1:2:end),invz)
+
+
+
+
+
+
+
+
+
+
+
+
--- Prec_vector.m
+++ Prec_vector.m
@@ -2,63 +2,66 @@ clear all
 close all 
 clc
 
-%% 2D
+%% xy plane
+t = linspace(0,60,1000);
 
-theta = linspace(0,10*pi,1000);
+f1 = 1;
+T1 = (2*pi)./f1;
 
-x = sin(theta);
-y = cos(theta);
+x = sin(f1.*t);
+y = cos(f1.*t);
 
-% figure(1)
-% plot(theta,x)
-% hold on
-% plot(theta,y)
+%% z direction
 
-% figure(2)
+T2 = max(t).*2; %period = double the timespan to go from -1 to 1 
+f2 = (2*pi)/T2; 
 
-% r = [x;y];
-% plot(r(1,:),r(2,:),'k*','LineWidth',10);
-% hold on
-% plot(r(1,2),r(2,2),'k*','LineWidth',10);
-% axis square
-
-% figure(3)
-% pl = quiver(0,0,r(1,1),r(2,1));
-% xlim([-1.5 1.5]); ylim([-1.5 1.5]);
-% axis square
-% 
-% for i = 1:length(theta)
-%     pl.UData = r(1,i);
-%     pl.VData = r(2,i);
-%     drawnow
-% end
-
-%% 3D
-
-z = sin(theta./2); % Height of vector
+z = -cos(f2.*t);
+figure(1)
+plot(t,z)
 
 or = [0 0 0]; %Origin
-
 r2 = [x;y;z;];
+%% Plotting
+
+%Angles
+theta = acosd(z);
+phi = atand(y./x);
+
 figure(10)
-pl2 = quiver3(or(1),or(2),or(3),r2(1,1),r2(2,1),r2(3,1),"LineWidth",3);
+
+subplot(2,2,[1 3])
+pl1 = quiver3(or(1),or(2),or(3),r2(1,1),r2(2,1),r2(3,1),"LineWidth",3);
 hold on
-pl3 = plot3(r2(1,1),r2(2,1),r2(3,1),'r-');
+pl2 = plot3(r2(1,1),r2(2,1),r2(3,1),'r-');
 xlim([-1.5 1.5]); ylim([-1.5 1.5]); zlim([-1.5 1.5])
 axis square
 
-for j = 1:length(theta)
-    pl2.UData = r2(1,j);
-    pl2.VData = r2(2,j);
-    pl2.WData = r2(3,j);
-    pl3.XData = r2(1,1:j);
-    pl3.YData = r2(2,1:j);
-    pl3.ZData = r2(3,1:j);
+subplot(2,2,2)
+pl3 = plot(t(1),phi(1));
+xlim([0 max(t)]); ylim([-95 95]);
+
+subplot(2,2,4)
+pl4 = plot(t(1),theta(1));
+xlim([0 max(t)]); ylim([0 190]); 
+
+for j = 1:length(t)
+    pl1.UData = r2(1,j);
+    pl1.VData = r2(2,j);
+    pl1.WData = r2(3,j);
+
+    pl2.XData = r2(1,1:j);
+    pl2.YData = r2(2,1:j);
+    pl2.ZData = r2(3,1:j);
+
+    pl3.XData = t(1:j);
+    pl3.YData = phi(1:j);
+
+    pl4.XData = t(1:j);
+    pl4.YData = theta(1:j);
+
     drawnow
 end
-
-
-
 
 
 
